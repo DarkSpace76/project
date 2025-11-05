d130650c1da3ef97a42344a0ba2566f8bd5dbec9
DarkSpace76 <84933249+DarkSpace76@users.noreply.github.com>
2025-11-05 20:06:57 +0300

    feat fb auth

---
+++ lib/styles/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+//Button style
+
+const LinearGradient btnBgGradient = LinearGradient(
+  begin: AlignmentDirectional.topCenter,
+  end: AlignmentDirectional.bottomCenter,
+  colors: [Color.fromRGBO(137, 36, 231, 1), Color.fromRGBO(106, 70, 249, 1)],
+);
+const Color btnDisable = Color.fromRGBO(135, 133, 143, 1);
+const Color btnBgWhite = Color.fromRGBO(238, 238, 238, 1);
+const Color textFillButton = Color.fromRGBO(19, 19, 19, 1);
+const Color textGradientButton = Colors.white;
+
+//CardColors
+const borderCardColor = Color.fromRGBO(135, 133, 143, 1);
+const cardTextColor = Color.fromRGBO(135, 133, 143, 1);
+const cardInputColor = Color.fromRGBO(135, 133, 143, 1);
+const hintColors = Color.fromRGBO(135, 133, 143, 1);
+
+const shadowdColor1 = Color.fromRGBO(227, 227, 227, 0.233);
+const shadowdColor2 = Color.fromRGBO(19, 19, 19, 1);
+
+const appbarShadowdColor1 = Color.fromRGBO(227, 227, 227, 0.233);
+const appBarShadowdColor2 = Color.fromRGBO(18, 7, 23, 1);
+const appbarColorTitle = Color.fromRGBO(238, 238, 238, 1);
